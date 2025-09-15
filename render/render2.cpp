fa28b2cb1d3330b59c80ae47957ad0f92537196c
BeguiledLeaf <joshthebob456@gmail.com>
2025-09-14 23:25:12 -0400

    Taking advantage of Classes

--- render/render2.cpp
+++ render/render2.cpp
@@ -16,14 +16,13 @@
 #include <sstream>
 #include <string>
 #include <cmath> 
-#include "render2.h"
+#include "render2.h" 
 
- /* Find max vector
-  * max: Given vector to hold maximum x,y,z values
-  * V: A structure holding all the given input vertices
-  * vertices: The amount of vertices in the input file
-  */
-void v_max(float max[3], const std::vector<Vector>& V, int vertices)
+/* Find max vector
+ * V: A structure holding all the given input vertices
+ * vertices: The amount of vertices in the input file
+ */
+Vector v_max(const std::vector<Vector>& V, int vertices)
 {
 	//Assign min variable to hold temporarily
 	float max_A = -FLOAT_MAX;
@@ -38,18 +37,15 @@ void v_max(float max[3], const std::vector<Vector>& V, int vertices)
 		if (max_C < V[i].c) max_C = V[i].c;
 	}
 
-	//Found max, assign into given array
-	max[0] = max_A;
-	max[1] = max_B;
-	max[2] = max_C;
+	//Found max, assign into given array 
+	return Vector(max_A, max_B, max_C);
 }
 
 /* Find min vector
- * min: Given vector to hold minimum x,y,z values
  * V: A structure holding all the given input vertices
  * vertices: The amount of vertices in the input file
  */
-void v_min(float min[3], const std::vector<Vector>& V, int vertices)
+Vector v_min(const std::vector<Vector>& V, int vertices)
 {
 	//Assign max variable to hold temporarily
 	float min_A = FLOAT_MAX;
@@ -64,37 +60,32 @@ void v_min(float min[3], const std::vector<Vector>& V, int vertices)
 		if (min_C > V[i].c) min_C = V[i].c;
 	}
 
-	//Found min, assign into given array
-	min[0] = min_A;
-	min[1] = min_B;
-	min[2] = min_C;
+	//Found min, assign into given array 
+	return Vector(min_A, min_B, min_C);
 }
 
 /* Find center vector (min + max)/2
- * center: Given vector to hold center x,y,z values
  * max: Given vector to hold maximum x,y,z values
  * min: Given vector to hold minimum x,y,z values
  */
-void v_center(float center[3], float max[3], float min[3])
+Vector v_center(Vector max, Vector min)
 {
-	center[0] = (max[0] + min[0]) / 2;
-	center[1] = (max[1] + min[1]) / 2;
-	center[2] = (max[2] + min[2]) / 2;
+	return Vector((max.a + min.a) / 2, (max.b + min.b) / 2, (max.c + min.c) / 2);
 }
 
 /* Find E scalar of bounding box, largest component of <max-min> or largest extent of the three axes
  * max: Given vertex to hold maximum x,y,z values
  * min: Given vertex to hold minimum x,y,z values
  */
-float find_e(float max[3], float min[3])
+float find_e(Vector max, Vector min)
 {
 	//Assign temporary value for e
 	float e = -FLOAT_MAX;
 
 	//Grab extents of each axis
-	float temp_X = max[0] - min[0];
-	float temp_Y = max[1] - min[1];
-	float temp_Z = max[2] - min[2];
+	float temp_X = max.a - min.a;
+	float temp_Y = max.b - min.b;
+	float temp_Z = max.c - min.c;
 
 	//Find largest extent 
 	e = temp_X;
@@ -132,22 +123,15 @@ void create_rotate(float R[3][3], float R1[3][3], float R2[3][3])
 /* Apply rotation by multiplying a 3x3 matrix by a 3x1 matrix. Output is a 3x1 matrix that is rotated around all axes (XYZ)
  * product: Our output matrix, stores the product of the two input matrices
  * R: Input 3x3 matrix. First matrix in the multiplication order
- * V: Input 3x1 matrix. Second matrix in the multiplication order
+ * V: Input 3x1 matrix. Second matrix in the multiplication order 
+ * Note: Wish I kept this in original C implementation, but doesn't hurt to practice
  */
-void v_rotate(float product[3], float R[3][3], float V[3])
-{
+void v_rotate(Vector product, float R[3][3], Vector V)
+{ 
 	//Go through entire 3x3 matrix. Go through each row at a time
-	for (int i = 0; i < 3; i++)
-	{
-		//Set to zero to allow for incrementation
-		product[i] = 0;
-
-		//Go through each column in the row, adding all the multiplications between the two matrices
-		for (int j = 0; j < 3; j++)
-		{
-			product[i] += R[i][j] * V[j];
-		}
-	}
+	product.a = (R[0][0] * V.a) + (R[0][1] * V.b) + (R[0][2] * V.c);
+	product.b = (R[1][0] * V.a) + (R[1][1] * V.b) + (R[1][2] * V.c);
+	product.c = (R[2][0] * V.a) + (R[2][1] * V.b) + (R[2][2] * V.c);
 }
 
 /* Rotate the camera & up vectors by the given degrees
@@ -157,7 +141,7 @@ void v_rotate(float product[3], float R[3][3], float V[3])
  * Y: Degrees that camera & up vectors will be rotated about the Y axis, given by user input
  * Z: Degrees that camera & up vectors will be rotated about the Z axis, given by user input
  */
-void rotate(float cam[3], float up[3], float X, float Y, float Z)
+void rotate(Vector cam, Vector up, float X, float Y, float Z)
 {
 	//Convert degree input into radians for cmath trig functions 
 	float radX = X * (PI / 180.0);
@@ -170,12 +154,12 @@ void rotate(float cam[3], float up[3], float X, float Y, float Z)
 	float Rz[3][3] = { {cos(radZ), -sin(radZ), 0}, {sin(radZ), cos(radZ), 0}, {0, 0, 1} };
 
 	//Create matrices to store products & old vectors for purposes of matrix multiplication
-	float R[3][3], temp_R[3][3], temp_cam[3], temp_up[3];
-	for (int i = 0; i < 3; i++) //Create temporary arrays to hold old cam/up vectors
-	{
-		temp_cam[i] = cam[i];
-		temp_up[i] = up[i];
-	}
+	float R[3][3], temp_R[3][3]; 
+
+	//Create temporary arrays to hold old cam/up vectors
+	Vector temp_cam = Vector(cam.a, cam.b, cam.c);  
+	Vector temp_up = Vector(up.a, up.b, up.c);
+
 
 	//Find rotation matrix Rz*(Ry*Rx)
 	create_rotate(temp_R, Ry, Rx); //Ry * Rx
@@ -186,69 +170,14 @@ void rotate(float cam[3], float up[3], float X, float Y, float Z)
 	v_rotate(up, R, temp_up);
 }
 
-/* Add two 3x1 matrices together
- * sum: Our output matrix, stores the sum of the two input matrices
- * v1: Input 3x1 matrix. First matrix in the addition order
- * v2: Input 3x1 matrix. Second matrix in the addition order
- */
-void v_add(float sum[3], float v1[3], float v2[3])
-{
-	//Go through matrix, add corresponding rows together
-	for (int i = 0; i < 3; i++)
-	{
-		sum[i] = v1[i] + v2[i];
-	}
-}
-
-/* Subtract two 3x1 matrices together
- * diff: Our output matrix, stores the difference of the two input matrices
- * v1: Input 3x1 matrix. First matrix in the subtraction order
- * v2: Input 3x1 matrix. Second matrix in the subtraction order
- */
-void v_sub(float diff[3], float v1[3], float v2[3])
-{
-	//Go through matrix, subtract corresponding rows together
-	for (int i = 0; i < 3; i++)
-	{
-		diff[i] = v1[i] - v2[i];
-	}
-}
-
-/* Multiply a 3x1 matrix by a scalar
- * product: Output 3x1 matrix, result of the scalar being applied to the matrix
- * v: Input 3x1 matrix
- * scal: Scalar that matrix will be multiplied by
- */
-void v_scal(float product[3], float v[3], float scal)
-{
-	//Go through matrix, multiply each row by scalar
-	for (int i = 0; i < 3; i++)
-	{
-		product[i] = v[i] * scal;
-	}
-}
-
-/* Cross Multiply two 3x1 matrices using Algebraic Definition
- * product: Result of the cross multiplication (3x1 matrix)
- * v1: Input 3x1 matrix
- * v2: Input 3x1 matrix
- */
-void v_cross_product(float product[3], float v1[3], float v2[3])
-{
-	//Apply cross product formula 
-	product[0] = (v1[1] * v2[2]) - (v1[2] * v2[1]); //v1[y]*v2[z] - v1[z]*v2[y]
-	product[1] = (v1[2] * v2[0]) - (v1[0] * v2[2]); //v1[z]*v2[x] - v1[x]*v2[z]
-	product[2] = (v1[0] * v2[1]) - (v1[1] * v2[0]); //v1[x]*v2[y] - v1[y]*v2[x]
-}
-
 /* Dot Multiply two 3x1 matrices
  * v1: Input 3x1 matrix
  * v2: Input 3x1 matrix
  * Return product: Result of the dot multiplication (Scalar)
  */
-float v_dot_product(float v1[3], float v2[3])
+float v_dot_product(Vector v1, Vector v2)
 {
-	return (v1[0] * v2[0]) + (v1[1] * v2[1]) + (v1[2] * v2[2]);
+	return (v1.a * v2.a) + (v1.b * v2.b) + (v1.c * v2.c);
 }
 
 /* Find vertices for a point on the triangle, depending on the vector index & vector table
@@ -256,34 +185,21 @@ float v_dot_product(float v1[3], float v2[3])
  * table: Table that stores all vertices from .ply file
  * index: Specific vector that the face from .ply file wants, to assign vertice for triangle edge
  */
-void find_vector(float v[3], const std::vector<Vector>& table, int index)
+Vector find_vector(const std::vector<Vector>& table, int index)
 {
-	v[0] = table[index].a;
-	v[1] = table[index].b;
-	v[2] = table[index].c;
-}
-
-/* Print out vector for debugging purposes
- * vector: Vector you want printed out
- */
-void v_print(float vector[3])
-{
-	for (int i = 0; i < 3; i++)
-	{
-		std::cout << vector[i];
-	}
-	std::cout << std::endl;
+	return Vector(table[index].a, table[index].b, table[index].c);
 }
 
 /* Handle user input & begin Triangle Rendering */
 int main(int argc, char* argv[])
 {
 	unsigned char pixel[ROWS][COLS];
-	int r, c, i, close_tri, vertices, faces;
-	float camera[3], up[3], X, Y, Z, max[3], min[3], center[3];
-	float E, a, left[3], right[3], top[3], bottom[3], top_left[3], diff[3];
-	float diff1[3], sum[3], image[3], ABC[3], v0[3], v1[3], v2[3], D, n, d;
-	float zBuffer, intersect[3], dot1, dot2, dot3, prod1[3], prod2[3];
+	int close_tri, vertices, faces; 
+	Vector camera, up, max, min, center, left, right, top, bottom; 
+	Vector top_left, diff, diff1, sum, image, ABC, v0, v1, v2; 
+	Vector intersect, prod1, prod2;
+	float X, Y, Z, E, a, D, n, d;
+	float zBuffer, dot1, dot2, dot3;
 
 	//Make sure user enters correct # of arguments
 	if (argc != 5)
@@ -333,10 +249,9 @@ int main(int argc, char* argv[])
 	//Create vertex & face structs to hold in vertices & faces from .ply file
 	std::vector <Vector> V(vertices);
 	std::vector <Face> triangle(faces); 
-	Vector();
 
 	//Collect vertices 
-	for (i = 0; i < vertices; i++)
+	for (int i = 0; i < vertices; i++)
 	{
 		infile >> V[i].a >> V[i].b >> V[i].c;
 	}
@@ -344,7 +259,7 @@ int main(int argc, char* argv[])
 	int temp; //Stores 3 in front of each triangle
 
 	//Collect faces (or triangles)
-	for (i = 0; i < faces; i++)
+	for (int i = 0; i < faces; i++)
 	{
 		infile >> temp >> triangle[i].v0 >> triangle[i].v1 >> triangle[i].v2;
 	}
@@ -353,10 +268,10 @@ int main(int argc, char* argv[])
 
 	/* Calculate the bounding box on the vertices */
 
-	v_max(max, V, vertices);
-	v_min(min, V, vertices);
-	v_center(center, max, min);
-	E = find_e(max, min);
+	max = v_max(V, vertices); 
+	min = v_min(V, vertices); 
+	center = v_center(max, min); 
+	E = find_e(max, min); 
 
 	/* Calculate camera position and orientation */
 
@@ -366,127 +281,123 @@ int main(int argc, char* argv[])
 	Z = atof(argv[4]);
 
 	//Default camera onto X axis (<1,0,0>) 
-	camera[0] = 1;
-	camera[1] = 0;
-	camera[2] = 0;
+	camera = Vector(1.0, 0.0, 0.0);
 
-	//Default up onto Z axis (<0,0,1>)
-	up[0] = 0;
-	up[1] = 0;
-	up[2] = 1;
+	//Default up onto Z axis (<0,0,1>) 
+	up = Vector(0.0, 0.0, 1.0);
 
 	//Rotate camera & up vector
 	rotate(camera, up, X, Y, Z);
 
 	//Move and scale the camera vector (<camera> = 1.5E<camera> + <center>) 
-	v_scal(camera, camera, 1.5 * E); //<camera> = 1.5E*<camera>
-	v_add(camera, camera, center); //<camera> = 1.5e*<camera> + center 
+	camera = camera * (1.5 * E); //<camera> = 1.5E*<camera> 
+	camera = camera + center; //<camera> = 1.5e*<camera> + center 
 
 	/* Determine the 3D coordinates bounding the image */
 
 	//Find first left
-	v_sub(diff, center, camera); //<diff> = <center> - <camera> 
-	v_cross_product(left, up, diff); //<left> = <up> x <center-camera> 
+	diff = center - camera;//<diff> = <center> - <camera> 
+	left = up.cross(diff); //<left> = <up> x <center-camera> 
 
 	//Find a = ||<left>||
-	a = sqrtf((left[0] * left[0]) + (left[1] * left[1]) + (left[2] * left[2]));
+	a = sqrtf((left.a * left.a) + (left.b * left.b) + (left.c * left.c));
 
 	//Find final left
-	v_scal(left, left, E / (2 * a)); //<left> = E/2a<left> 
-	v_add(left, left, center); //<left> = E/2a<left> + center  
+	left = left * (E / (2 * a)); //<left> = E/2a<left> 
+	left = left + center;//<left> = E/2a<left> + center  
 
 	//Find right
-	v_cross_product(right, diff, up); //right = <center-camera> x <up> 
-	v_scal(right, right, E / (2 * a)); //<right> = E/2a<right> 
-	v_add(right, right, center); //<right> = E/2a<right> + center 
+	right = diff.cross(up); //right = <center-camera> x <up> 
+	right = right * (E / (2 * a)); //<right> = E/2a<right> 
+	right = right + center;//<right> = E/2a<right> + center 
 
 	//Find top 
-	v_scal(top, up, E / 2); //<top> = E/2<up> 
-	v_add(top, top, center); //<top> = E/2<up> + <center> 
+	top = up * (E / 2); //<top> = E/2<up> 
+	top = top + center;//<top> = E/2<up> + <center> 
 
 	//Find bottom 
-	v_scal(bottom, up, (-E) / 2); //<bottom> = -E/2<up> 
-	v_add(bottom, bottom, center); //<bottom> = -E/2<up> + <center> 
+	bottom = up * ((-E) / 2); //<bottom> = -E/2<up> 
+	bottom = bottom + center;//<bottom> = -E/2<up> + <center> 
 
 	//Find topleft 
-	v_scal(top_left, up, E / 2); //<topleft> = E/2<up> 
-	v_add(top_left, top_left, left); //<topleft> = E/2<up> + <left> 
+	top_left = up * (E / 2); //<topleft> = E/2<up> 
+	top_left = top_left + left;//<topleft> = E/2<up> + <left> 
 
 	/* Determine each pixel r,c in the image */
 	std::cout << "Rendering..." << std::endl;
-	for (r = 0; r < ROWS; r++)
+	for (int r = 0; r < ROWS; r++)
 	{
 		//Go through each column in the row
-		for (c = 0; c < COLS; c++)
+		for (int c = 0; c < COLS; c++)
 		{
 			//Calculate vector coordinates for the image pixel
-			v_sub(diff, bottom, top); //<diff> = <bottom - top> 
-			v_scal(diff, diff, (float)r / (ROWS - 1)); //r/(ROWS-1)<bottom - top> 
-			v_sub(diff1, right, left); //<diff1> = <right-left>  
-			v_scal(diff1, diff1, (float)c / (COLS - 1)); //<diff1> = c/(COLS-1)<right-left> 
-			v_add(sum, diff1, diff); //<sum> = c/(COLS-1)<right-left> + r/(ROWS-1)<bottom - top>
-			v_add(image, top_left, sum); //<image> = <topleft> + c/(COLS-1)<right-left> + r/(ROWS-1)<bottom - top>  
+		    diff = bottom - top; //<diff> = <bottom - top> 
+			diff = diff * ((float)r / (ROWS - 1)); //[r/(ROWS-1)]<bottom - top> 
+			diff1 = right - left; //<diff1> = <right-left>  
+			diff1 = diff1 * ((float)c / (COLS - 1)); //<diff1> = c/(COLS-1)<right-left> 
+			sum = diff1 + diff;//<sum> = c/(COLS-1)<right-left> + r/(ROWS-1)<bottom - top>
+			image = top_left + sum;//<image> = <topleft> + c/(COLS-1)<right-left> + r/(ROWS-1)<bottom - top>  
 
 			//Find the plane equation that contains the triangle 
 			zBuffer = FAR; //Default the zBuffer to far away for each pixel before checking triangles 
 			close_tri = -1; //Default index to impossible value, meant to distinguish if any triangle is found or not
-			for (i = 0; i < faces; i++)
+			for (int i = 0; i < faces; i++)
 			{
 				//Find v0, v1, v2 for this triangle 
-				find_vector(v0, V, triangle[i].v0);
-				find_vector(v1, V, triangle[i].v1);
-				find_vector(v2, V, triangle[i].v2);
+				v0 = find_vector(V, triangle[i].v0);
+				v1 = find_vector(V, triangle[i].v1);
+				v2 = find_vector(V, triangle[i].v2); 
 
 				//Find ABC
-				v_sub(diff, v1, v0); //<diff> = <v1-v0> 
-				v_sub(diff1, v2, v0); //<diff1> = <v2-v0> 
-				v_cross_product(ABC, diff, diff1); //<A,B,C> = <v1-v0> x <v2-v0> 
+				diff = v1 - v0; //<diff> = <v1-v0> 
+				diff1 = v2 - v0; //<diff1> = <v2-v0> 
+				ABC = diff.cross(diff1); //<A,B,C> = <v1-v0> x <v2-v0> 
 
 				//Find D 
-				v_scal(prod1, ABC, -1); //<prod1> = -<A,B,C>
+				prod1 = ABC * -1; //<prod1> = -<A,B,C>
 				D = v_dot_product(prod1, v0); //D = -<A,B,C> * <v0>
 
 				//Find distance along the image pixel ray to the triangle
 				n = v_dot_product(prod1, camera) - D; //n = -<A,B,C> * camera - D
-				v_sub(diff, image, camera); //<diff> = <image - camera>
+				diff = image - camera; //<diff> = <image - camera>
 				d = v_dot_product(ABC, diff); //d = <ABC> * <image - camera> 
 
 				//If ray is parallel to triangle, d near zero, skip
 				if (fabs(d) > 1e-6)
 				{
 					//Find 3D coords <intersect> of ray & plane
-					v_scal(diff, diff, n / d); //<diff> = n/d<image-camera> 
-					v_add(intersect, camera, diff); //<intersect> = <camera> + n/d<image-camera> 
+					diff = diff * (n/d); //<diff> = n/d<image-camera> 
+					intersect = camera + diff;//<intersect> = <camera> + n/d<image-camera> 
 
 					/* Determine if intersection point lies within triangle, if any dot product is less than 0 (outside!), stop early */
 
 					//Dot1
-					v_sub(diff, v2, v0); //<diff> = <v2-v0> 
-					v_sub(diff1, v1, v0); //<diff1> = <v1-v0> 
-					v_cross_product(prod1, diff, diff1); //<prod1> = <v2-v0> x <v1-v0> 
-					v_sub(diff, intersect, v0); //<diff> = <intersect-v0> 
-					v_cross_product(prod2, diff, diff1); //<prod2> = <intersect-v0> x <v1-v0>
+					diff = v2 - v0; //<diff> = <v2-v0> 
+					diff1 = v1 - v0; //<diff1> = <v1-v0> 
+					prod1 = diff.cross(diff1); //<prod1> = <v2-v0> x <v1-v0> 
+					diff = intersect - v0; //<diff> = <intersect-v0> 
+					prod2 = diff.cross(diff1); //<prod2> = <intersect-v0> x <v1-v0>
 					dot1 = v_dot_product(prod1, prod2); //dot1 = 〈v2 − v0〉 × 〈v1 − v0〉 · 〈intersect − v0〉 × 〈v1 − v0〉 
 
 					//Dot2 if previous passes
 					if (dot1 >= 0)
 					{
-						v_sub(diff, v0, v1); //<diff> = <v0-v1> 
-						v_sub(diff1, v2, v1); //<diff1> = <v2-v1> 
-						v_cross_product(prod1, diff, diff1); //<prod1> = <v0-v1> x <v2-v1> 
-						v_sub(diff, intersect, v1); //<diff> = <intersect-v1> 
-						v_cross_product(prod2, diff, diff1); //<prod2> = <intersect-v1> x <v2-v1>
+						diff = v0 - v1; //<diff> = <v0-v1> 
+						diff1 = v2 - v1; //<diff1> = <v2-v1> 
+						prod1 = diff.cross(diff1); //<prod1> = <v0-v1> x <v2-v1> 
+						diff = intersect - v1; //<diff> = <intersect-v1> 
+						prod2 = diff.cross(diff1); //<prod2> = <intersect-v1> x <v2-v1>
 						dot2 = v_dot_product(prod1, prod2); //dot2 = 〈v0 − v1〉 × 〈v2 − v1〉 · 〈intersect − v1〉 × 〈v2 − v1〉
 					}
 
 					//Dot3 if previous two pass
 					if (dot1 >= 0 && dot2 >= 0)
 					{
-						v_sub(diff, v1, v2); //<diff> = <v1-v2> 
-						v_sub(diff1, v0, v2); //<diff1> = <v0-v2> 
-						v_cross_product(prod1, diff, diff1); //<prod1> = <v1-v2> x <v0-v2> 
-						v_sub(diff, intersect, v2); //<diff> = <intersect-v2> 
-						v_cross_product(prod2, diff, diff1); //<prod2> = <intersect-v2> x <v0-v2>
+						diff = v1 - v2; //<diff> = <v1-v2> 
+						diff1 = v0 - v2; //<diff1> = <v0-v2> 
+						prod1 = diff.cross(diff1); //<prod1> = <v1-v2> x <v0-v2> 
+						diff = intersect - v2; //<diff> = <intersect-v2> 
+						prod2 = diff.cross(diff1); //<prod2> = <intersect-v2> x <v0-v2>
 						dot3 = v_dot_product(prod1, prod2); //dot3 = 〈v1 − v2〉 × 〈v0 − v2〉 · 〈intersect − v2〉 × 〈v0 − v2〉
 					}
 
--- render/render2.h
+++ render/render2.h
@@ -21,9 +21,54 @@ class Vector{
 public:
 	float a, b, c; 
 
-	//Constructors
+	/* Constructors */
 	Vector() : a(0.0), b(0.0), c(0.0) {} 
-	Vector(float _a, float _b, float _c) : a(_a), b(_b), c(_c) {}
+	Vector(float _a, float _b, float _c) : a(_a), b(_b), c(_c) {} 
+
+	/* Member Functions Declarations */ 
+
+	// Add two 3x1 matrices together. Overload + operator
+	Vector operator+(const Vector& other) const 
+	{
+		//Go through matrix, add corresponding rows together
+		return Vector(a + other.a, b + other.b, c + other.c);
+	}
+
+	// Subtract two 3x1 matrices together
+	Vector operator-(const Vector& other) const
+	{
+		//Go through matrix, subtract corresponding rows together
+		return Vector(a - other.a, b - other.b, c - other.c);
+	}
+
+	// Multiply a 3x1 matrix by a scalar
+	Vector operator*(float scal) const
+	{
+		//Go through matrix, multiply each row by scalar 
+		return Vector(a * scal, b * scal, c * scal);
+	}
+
+	/* Cross Multiply two 3x1 matrices using Algebraic Definition
+	 * product: Result of the cross multiplication (3x1 matrix)
+	 * v1: Input 3x1 matrix
+	 * v2: Input 3x1 matrix
+	 */
+	Vector cross(const Vector& other) const
+	{
+		//Apply cross product formula 
+		Vector product;
+		product.a = (b * other.c) - (c * other.b); //v1[y]*v2[z] - v1[z]*v2[y]
+		product.b = (c * other.a) - (a * other.c); //v1[z]*v2[x] - v1[x]*v2[z]
+		product.c = (a * other.b) - (b * other.a); //v1[x]*v2[y] - v1[y]*v2[x] 
+		return product;
+	} 
+
+	// Print out vector for debugging purposes
+	void print() const
+	{
+		std::cout << a << ", " << b << ", " << c << std::endl;
+	}
+
 };
 
 //Class holds the faces of a 3x1 matrix. Meant to store triangle points
@@ -33,21 +78,19 @@ public:
 
 	//Constructors
 	Face() : v0(0), v1(0), v2(0) {}
-	Face(int _v0, int _v1, int _v2) : v0(_v0), v1(_v1), v2(_v2) {}
+	Face(int _v0, int _v1, int _v2) : v0(_v0), v1(_v1), v2(_v2) {} 
+
+	void print() const
+	{
+		std::cout << v0 << ", " << v1 << ", " << v2 << std::endl;
+	}
 };
 
-/* Function Declarations */
-void v_max(float max[3], const std::vector<Vector>& V, int vertices); //Find max vertices for XYZ plane
-void v_min(float min[3], const std::vector<Vector>& V, int vertices); //Find min vertices for XYZ plane
-void v_center(float center[3], float max[3], float min[3]); //Find center vertices for XYZ plane
-float find_e(float max[3], float min[3]); //Find E scalar
+/* Function Declarations */ 
+float find_e(Vector max, Vector min); //Find E scalar
 void create_rotate(float R[3][3], float R1[3][3], float R2[3][3]); //Create the 3x3 rotation matrix for XYZ plane
-void v_rotate(float product[3], float R[3][3], float V[3]); //Apply rotation onto given 3x1 vector
-void rotate(float cam[3], float up[3], float X, float Y, float Z); //Handle rotation of camera & up vector
-void v_add(float sum[3], float v1[3], float v2[3]); //Add two 3x1 vectors together
-void v_sub(float diff[3], float v1[3], float v2[3]); //Subtract two 3x1 vectors together
-void v_scal(float product[3], float v[3], float scal); //Apply scalar to 3x1 vector
-void v_cross_product(float product[3], float v1[3], float v2[3]); //Cross Product two 3x1 vectors
-float v_dot_product(float v1[3], float v2[3]); //Dot Product two 3x1 vectors
-void find_vector(float v[3], const std::vector<Vector>& table, int index); //Find vertices for given point on triangle
-void v_print(float vector[3]); //Print out 3x1 vector#pragma once
+void v_rotate(Vector product, float R[3][3], Vector V); //Apply rotation onto given 3x1 vector
+void rotate(Vector cam, Vector up, float X, float Y, float Z); //Handle rotation of camera & up vector
+float v_dot_product(Vector v1, Vector v2); //Dot Product two 3x1 vectors
+Vector find_vector(const std::vector<Vector>& table, int index); //Find vertices for given point on triangle
+#pragma once
